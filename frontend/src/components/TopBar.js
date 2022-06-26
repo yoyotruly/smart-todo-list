085dcd185b573fcee28f6047a6476f629990dd2f
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-26 13:35:03 -0400

    Add search to top bar

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,11 +1,52 @@
+import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
 const drawerWidth = 240;
 
-export default function TopBar() {
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
 
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function TopBar() {
   return (
     <AppBar
       position="fixed"
@@ -14,16 +55,26 @@ export default function TopBar() {
         ml: `${drawerWidth}px`,
         zIndex: (theme) => theme.zIndex.drawer + 1
       }}
-      >
-      <Toolbar>
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-        >
-          Top Bar
-        </Typography>
+    >
+
+      <Toolbar disableGutters>
+
+        <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+        </Search>
+
       </Toolbar>
+
     </AppBar>
   )
 }
+
+
+
+
