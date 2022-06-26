8c412b4066aad7f9ba2035528f7aeee615f928b5
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-26 04:00:18 -0400

    Add template sidebar

--- frontend/src/pages/tasks/Tasks.js
+++ frontend/src/pages/tasks/Tasks.js
@@ -1,6 +1,11 @@
+import Sidebar from "../../components/Sidebar"
+
 function Tasks() {
   return (
-    <div>This is tasks overview page</div>
+    <div>
+      <Sidebar />
+      This is tasks overview page
+    </div>
   )
 }
 
