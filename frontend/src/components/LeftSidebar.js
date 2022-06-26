9c94726c6e1a26dd3cfd4d2b065fff08f001b685
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-26 13:25:24 -0400

    Modify list icons and texts

--- frontend/src/components/LeftSidebar.js
+++ frontend/src/components/LeftSidebar.js
@@ -5,8 +5,12 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
+import TodayOutlinedIcon from '@mui/icons-material/TodayOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import AccessAlarmOutlinedIcon from '@mui/icons-material/AccessAlarmOutlined';
+import EventAvailableOutlinedIcon from '@mui/icons-material/EventAvailableOutlined';
+import SquareRoundedIcon from '@mui/icons-material/SquareRounded';
 
 const leftSidebarWidth = 240;
 
@@ -28,31 +32,93 @@ export default function LeftSidebar() {
         <Toolbar />
 
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text}>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+
+          <ListItem key={"Overview"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <SummarizeOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Overview"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={"Today"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <TodayOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Today"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={"Scheduled"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <AccessAlarmOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Scheduled"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={"Important"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <StarBorderOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Important"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={"Done"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <EventAvailableOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Done"} />
+            </ListItemButton>
+          </ListItem>
+
         </List>
 
         <List
           sx={{marginTop: "30px"}}
         >
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text}>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          <ListItem key={"To Eat"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <SquareRoundedIcon color='primary'/>
+              </ListItemIcon>
+              <ListItemText primary={"To Eat"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={"To Buy"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <SquareRoundedIcon color="warning"/>
+              </ListItemIcon>
+              <ListItemText primary={"To Buy"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={"To Read"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <SquareRoundedIcon color="secondary"/>
+              </ListItemIcon>
+              <ListItemText primary={"To Read"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem key={"To Watch"}>
+            <ListItemButton>
+              <ListItemIcon>
+                <SquareRoundedIcon color="success"/>
+              </ListItemIcon>
+              <ListItemText primary={"To Watch"} />
+            </ListItemButton>
+          </ListItem>
+
         </List>
       </Drawer>
   );
