df3ec31e1482614ec9314943429a80540310cf1e
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-26 17:02:30 -0400

    Add floating add button

---
+++ frontend/src/components/FloatingAddButton.js
@@ -0,0 +1,19 @@
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
+export default function FloatingActionButtons() {
+  return (
+    <Fab
+      color="primary"
+      aria-label="add"
+      sx={{
+        position: "absolute",
+        left: 20,
+        bottom: 20,
+        zIndex: (theme) => theme.zIndex.drawer + 1
+      }}
+    >
+      <AddIcon />
+    </Fab>
+  );
+}
--- frontend/src/pages/tasks/Tasks.js
+++ frontend/src/pages/tasks/Tasks.js
@@ -3,11 +3,13 @@ import Box from '@mui/material/Box';
 import TopBar from "../../components/TopBar"
 import LeftSidebar from "../../components/LeftSidebar"
 import MainContent from '../../components/MainContent';
+import FloatingAddButton from '../../components/FloatingAddButton';
 
 function Tasks() {
   return (
     <Box sx={{ display: 'flex' }}>
 
+      <FloatingAddButton />
       <TopBar />
       <LeftSidebar />
       <MainContent />
