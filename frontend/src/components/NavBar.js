98360d33f250fa700fbf96947abdc6401fa61739
Winston Limanto <wmlimanto@gmail.com>
2022-06-27 21:25:00 +0700

    update ref link for login button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -24,7 +24,7 @@ export default function NavBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" href="/login">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
