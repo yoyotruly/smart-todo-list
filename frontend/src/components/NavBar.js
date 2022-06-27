6d4dd8663192e60d0e3d5b88d4fa10018ee07b65
Winston Limanto <wmlimanto@gmail.com>
2022-06-27 21:22:41 +0700

    update nav bar position to sticky -- so it remains on top

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,7 +10,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="sticky">
         <Toolbar>
           <IconButton
             size="large"
