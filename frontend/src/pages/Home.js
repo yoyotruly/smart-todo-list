dbf20b7a1d9646ba5f06aeefe71280241cf123cb
Winston Limanto <wmlimanto@gmail.com>
2022-06-29 16:38:00 +0700

    update homepage layout and add hero image

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -30,14 +30,14 @@ export default function Register() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container spacing={2} columns={16}>
         <CssBaseline />
-       
+
         <Grid
           item
-          xs={false}
-          sm={4}
-          md={7}
+          xs={8}
+          // sm={4}
+          // md={7}
           direction='column'
           justifyContent='center'
           alignItems='center'
@@ -46,7 +46,7 @@ export default function Register() {
           square>
             <Box
             sx={{
-              my: 50,
+              my: 40,
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
@@ -66,71 +66,23 @@ export default function Register() {
             </Box>
         </Grid>
 
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up for a free account
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="/login" variant="body2">
-                    {"Already have an account? Sign In"}
-                  </Link>
-                </Grid>
-              </Grid>
-
-            </Box>
-          </Box>
-        </Grid>
+        <Grid
+          item
+          xs={8}
+          direction='column'
+          justifyContent='center'
+          alignItems='center'
+          minHeight='100vh'
+          textAlign='center'
+          square
+          sx={{
+            backgroundImage: 'url(./homepage-hero.png)',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
       </Grid>
     </ThemeProvider>
   );
