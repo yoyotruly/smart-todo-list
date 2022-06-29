10cf55a3aeaf3060c8a8aaf54221e673cc08cdac
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-28 20:43:36 -0400

    Reorder label seed data

--- backend/db/seeds/03_labels.sql
+++ backend/db/seeds/03_labels.sql
@@ -1,4 +1,4 @@
-INSERT INTO labels (label) VALUES ('To Watch');
 INSERT INTO labels (label) VALUES ('To Eat');
-INSERT INTO labels (label) VALUES ('To Read');
 INSERT INTO labels (label) VALUES ('To Buy');
+INSERT INTO labels (label) VALUES ('To Read');
+INSERT INTO labels (label) VALUES ('To Watch');
