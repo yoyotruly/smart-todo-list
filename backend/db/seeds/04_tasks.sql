c3d6b69a9e191e9c7bbb8f0b5e6f3942cc92b83e
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-30 07:25:02 -0400

    Add seed data for tasks table

---
+++ backend/db/seeds/04_tasks.sql
@@ -0,0 +1,31 @@
+INSERT INTO tasks
+  (user_id, priority_id, label_id, title, description, due_date)
+VALUES
+  (1, 1, 2, 'Buy ugly vegetables from the Farmer''s Market this weekend', 'People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too. I need to save them.\n(Wow, I''m such a hero)', '2022-07-02');
+
+INSERT INTO tasks
+  (user_id, priority_id, label_id, title, description, is_complete)
+VALUES
+  (1, 2, 4, 'Rewatch Hawk and Chick with Louise', 'Best. Movie. Ever.', TRUE);
+
+INSERT INTO tasks
+  (user_id, label_id, title, description)
+VALUES
+  (1, 3, 'Test Title', 'Test description.');
+
+INSERT INTO tasks
+  (user_id, label_id, title, description)
+VALUES
+  (1, 3, 'Test Title', 'Test description.');
+
+INSERT INTO tasks
+  (user_id, label_id, title, description)
+VALUES
+  (1, 4, 'Test Title', 'Test description.');
+
+INSERT INTO tasks
+  (user_id, label_id, title, description)
+VALUES
+  (1, 4, 'Test Title', 'Test description.');
+
+
