c5d915f344eabf3e52c81b3ba4ed1317b18f70d6
Winston Limanto <wmlimanto@gmail.com>
2022-06-30 18:10:11 +0700

    update updateTaskById function

--- backend/models/model.task.js
+++ backend/models/model.task.js
@@ -55,19 +55,60 @@ const getTaskById = (id) => {
     .catch(err => console.log(err.stack));
 };
 
-const updateTaskById = (id) => {
-  const query = `
+const updateTaskById = (id, taskData) => {
+  let query = `
     UPDATE tasks
-       SET -- insert fields
-     WHERE id = $1
-    RETURNING *;
-  `;
-  const param = [id];
+    SET `;
+  const param = [];
+  if(taskData.priority_id) {
+    param.push(taskData.priority_id)
+    query += `priority_id = $${param.length}`
+  }
+  if(taskData.label_id) {
+    param.push(taskData.label_id)
+    param.length < 1 ?
+    query += `label_id = $${param.length}` :
+    query += `, label_id = $${param.length}`
+  }
+  if(taskData.title) {
+    param.push(taskData.title)
+    param.length < 1 ?
+    query += `title = $${param.length}` :
+    query += `, title = $${param.length}`
+  }
+  if(taskData.description) {
+    param.push(taskData.description)
+    param.length < 1 ?
+    query += `description = $${param.length}` :
+    query += `, description = $${param.length}`
+  }
+  if(taskData.due_date) {
+    param.push(taskData.due_date)
+    param.length < 1 ?
+    query += `due_date = $${param.length}` :
+    query += `, due_date = $${param.length}`
+  }
+  if(taskData.is_complete) {
+    param.push(taskData.is_complete)
+    param.length < 1 ?
+    query += `is_complete = $${param.length}` :
+    query += `, is_complete = $${param.length}`
+  }
+  if(taskData.is_active) {
+    param.push(taskData.is_active)
+    param.length < 1 ?
+    query += `is_active = $${param.length}` :
+    query += `, is_active = $${param.length}`
+  }
+  param.push(id);
+  query += ` WHERE id = $${param.length}
+    RETURNING *;`;
+  console.log(query);
 
   return db
     .query(query, param)
     .then(res => {
-      const task = task.rows[0];
+      const task = res.rows[0];
       if (!task) return null;
       return task;
     })
