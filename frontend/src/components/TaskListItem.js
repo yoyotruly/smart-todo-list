1ddb5ce059e5b1a3dadda4ce2e678fbb241b0750
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 14:28:26 -0400

    Add category options to task list item

--- frontend/src/components/TaskListItem.js
+++ frontend/src/components/TaskListItem.js
@@ -4,9 +4,13 @@ import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import CircleCheckbox from './CircleCheckbox';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
-import Divider from '@mui/material/Divider';
+import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
+import TagRoundedIcon from '@mui/icons-material/TagRounded';
+import EventOutlinedIcon from '@mui/icons-material/EventOutlined';
+import grey from '@mui/material/colors/grey';
 
 
 export default function TaskListItem(props) {
@@ -23,9 +27,33 @@ export default function TaskListItem(props) {
               <Typography variant="body1" color="text.primary" sx={{mb: "5px"}}>
                 {props.title}
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {props.due_date} | {props.priority} | {props.label}
-              </Typography>
+
+              <Box sx={{display: "flex", gap: "20px", marginTop: "10px"}}>
+
+                <Box sx={{display: "flex", alignItems: "center", gap: "3px"}}>
+                  <EventOutlinedIcon sx={{ color: grey[700], fontSize: "15px" }} />
+                  <Typography variant="caption" color="text.secondary">
+                    {props.due_date}
+                  </Typography>
+                </Box>
+
+                <Box sx={{display: "flex", alignItems: "center", gap: "3px"}}>
+                  <FlagOutlinedIcon sx={{ color: grey[700], fontSize: "15px" }} />
+                  <Typography variant="caption" color="text.secondary">
+                    {props.priority}
+                  </Typography>
+                </Box>
+
+
+                <Box sx={{display: "flex", alignItems: "center", gap: "3px"}}>
+                  <TagRoundedIcon sx={{ color: grey[700], fontSize: "15px" }} />
+                  <Typography variant="caption" color="text.secondary" >
+                    {props.label}
+                  </Typography>
+                </Box>
+
+              </Box>
+
             </CardContent>
           </CardActionArea>
         </Box>
