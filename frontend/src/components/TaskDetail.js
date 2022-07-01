6a43e603b2a5326746d8fe495207065efa5060bc
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 02:03:34 -0400

    Add more list items to task detail

--- frontend/src/components/TaskDetail.js
+++ frontend/src/components/TaskDetail.js
@@ -1,38 +1,18 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import Container from '@mui/system/Container';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { Container } from '@mui/system';
+import CircleCheckbox from './CircleCheckbox';
+import AddIcon from '@mui/icons-material/Add';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
-export default function RecipeReviewCard() {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+export default function RecipeReviewCard(props) {
   return (
     <Container>
       <Card
@@ -40,82 +20,62 @@ export default function RecipeReviewCard() {
         sx={{marginTop: "25px"}}
       >
         <CardHeader
-          avatar={
-            <ListItemIcon >
-              <CheckCircleIcon
-                fontSize="large"
-                color="primary"
-              />
-            </ListItemIcon>
-          }
+          avatar={<CircleCheckbox />}
           action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
+            <div>
+              <IconButton aria-label="add to high priority">
+                <StarBorderIcon />
+              </IconButton>
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            </div>
           }
           title={
             <Typography variant="body1" color="text.primary">
-              Buy ugly vegetables from the Farmer's Market this weekend
+              {props.task.title}
             </Typography>
           }
-          subheader="Jan 7, 2022"
+          subheader={props.task.due_date}
         />
 
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too.
-            I need to save them.
-          </Typography>
-          <br />
-          <Typography variant="body2" color="text.secondary">
-            (Wow, I'm such a hero)
+            {props.task.description}
           </Typography>
         </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon color="error"/>
-          </IconButton>
-          <IconButton aria-label="add to high priority">
-            <StarBorderIcon />
-          </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-              aside for 10 minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-              medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-              occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-              large plate and set aside, leaving chicken and chorizo in the pan. Add
-              pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-              stirring often until thickened and fragrant, about 10 minutes. Add
-              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and
-              peppers, and cook without stirring, until most of the liquid is absorbed,
-              15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-              mussels, tucking them down into the rice, and cook again without
-              stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don&apos;t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
-          </CardContent>
-        </Collapse>
+
+        <List dense>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <AddIcon color="primary" />
+              </ListItemIcon>
+              <ListItemText primary="Add step" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <RadioButtonUncheckedIcon fontSize="small" color="disabled" />
+              </ListItemIcon>
+              <ListItemText primary="First step"/>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <RadioButtonUncheckedIcon fontSize="small" color="disabled" />
+              </ListItemIcon>
+              <ListItemText primary="Second step" />
+            </ListItemButton>
+          </ListItem>
+
+        </List>
+
       </Card>
     </Container>
   );
