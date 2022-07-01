55d7d25386eed6a07b52ab6d77ccf5841aed12ae
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 16:54:33 -0400

    Move tasks data to Main Content

--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -48,23 +48,43 @@ const appbarStyles = {
   borderColor: "primary.main",
 }
 
-const task = {
-  id: 1,
-  title: "Buy ugly vegetables from the Farmer's Market this weekend",
-  description: `People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too. I need to save them. (Wow, I'm such a hero)`,
-  due_date: "July 2, 2022",
-  priority: "High",
-  label: "To Buy"
-}
+const tasks = [
+  {
+    id: 1,
+    title: "Buy ugly vegetables from the Farmer's Market this weekend",
+    description: "People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too. I need to save them.\n (Wow, I'm such a hero)",
+    due_date: "July 2, 2022",
+    priority: "High",
+    label: "To Buy"
+  },
+  {
+    id: 2,
+    title: "Rewatch Hawk and Chick with Louise",
+    due_date: "July 2, 2022",
+    priority: "High",
+    label: "To Watch"
+  },
+  {
+    id: 3,
+    title: "test",
+    due_date: "July 2, 2022",
+    priority: "High",
+    label: "To Buy"
+  }
+];
 
 export default function RightSidebar() {
   const [isOpen, setOpen] = React.useState(false);
-  const toggle = () => setOpen(prevIsOpen => !prevIsOpen);
+  const toggle = (id) => {
+    console.log(id);
+    return setOpen(prevIsOpen => !prevIsOpen)
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
       <Main open={isOpen}>
         <TaskList
+          tasksData={tasks}
           listStyle={taskListStyles}
           appbarStyle={appbarStyles}
           toggle={toggle}
@@ -85,7 +105,7 @@ export default function RightSidebar() {
       >
 
         <Toolbar />
-        <TaskDetail task={task}/>
+        <TaskDetail task={tasks[0]}/>
 
       </Drawer>
     </Box>
--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -5,46 +5,18 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
-const tasks = [
-  {
-    id: 1,
-    title: "Buy ugly vegetables from the Farmer's Market this weekend",
-    description: "People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too. I need to save them.\n (Wow, I'm such a hero)",
-    due_date: "July 2, 2022",
-    priority: "High",
-    label: "To Buy"
-  },
-  {
-    id: 2,
-    title: "Rewatch Hawk and Chick with Louise",
-    due_date: "July 2, 2022",
-    priority: "High",
-    label: "To Watch"
-  },
-  {
-    id: 3,
-    title: "test",
-    due_date: "July 2, 2022",
-    priority: "High",
-    label: "To Buy"
-  }
-];
+export default function TaskList(props) {
 
-const createTaskElements = (tasks, props) => {
-  return tasks.map(task => (
+  const taskElements = props.tasksData.map(task => (
     <TaskListItem
       key={task.id}
       title={task.title}
       due_date={task.due_date}
       priority={task.priority}
       label={task.label}
-      toggle={props.toggle}
+      toggle={() => props.toggle(task.id)}
     />
   ))
-}
-
-export default function TaskList(props) {
-  const taskElements = createTaskElements(tasks, props)
 
   return (
     <>
