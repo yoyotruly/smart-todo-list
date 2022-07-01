e465eaf6e7de3d54e8a8837ee6fea31835adec69
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 01:02:10 -0400

    Remove Profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import CssBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import User from './components/User';
-import Profile from "./pages/Profile"
-import Tasks from "./pages/tasks/Tasks"
-import TaskDone from "./pages/tasks/TaskDone"
-import TaskHighPriority from "./pages/tasks/TaskHighPriority"
-import TaskLabel from "./pages/tasks/TaskLabel"
+import Tasks from "./pages/tasks/Tasks";
+import TaskDone from "./pages/tasks/TaskDone";
+import TaskHighPriority from "./pages/tasks/TaskHighPriority";
+import TaskLabel from "./pages/tasks/TaskLabel";
 
 function App() {
   return (
@@ -14,12 +13,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/*" element={<User />} />
-          <Route path="profile/:userId" element={<Profile />} />
           <Route path="tasks">
             <Route index element={<Tasks />} />
             <Route path=":labelId" element={<TaskLabel />} />
             <Route path="done" element={<TaskDone />} />
-            <Route path="high" element={<TaskHighPriority />} />
+            <Route path="important" element={<TaskHighPriority />} />
           </Route>
         </Routes>
       </Router>
