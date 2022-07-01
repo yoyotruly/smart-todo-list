bc8f987cfad35daba97d44f878bc151fc1162976
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 02:03:49 -0400

    Add task description to mock data

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -6,6 +6,7 @@ const tasks = [
   {
     id: 1,
     title: "Buy ugly vegetables from the Farmer's Market this weekend",
+    description: "People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too. I need to save them.\n (Wow, I'm such a hero)",
     due_date: "July 2, 2022",
     priority: "High",
     label: "To Buy"
