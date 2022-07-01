f895b6c3662c5766064b2d85be500496440fb246
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 11:09:27 -0400

    Refactor TaskListItem to use card component instead

--- frontend/src/components/TaskListItem.js
+++ frontend/src/components/TaskListItem.js
@@ -1,29 +1,43 @@
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import CircleCheckbox from './CircleCheckbox';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import Divider from '@mui/material/Divider';
+
 
 export default function TaskListItem(props) {
-  return (
-    <div>
-      <ListItem
-        alignItems="flex-start"
-        onClick={props.toggle}
-      >
-        <CircleCheckbox />
-        <ListItemText
-          primary={props.title}
-          secondary={
-            <>
-              <span>{props.due_date}</span>
-              <span> | Priority {props.priority}</span>
-              <span> | Label {props.label}</span>
-            </>
-          }
-        />
-      </ListItem>
-      <Divider component="li" />
-    </div>
-  )
-}
 
+  return (
+    <Card elevation={0} >
+
+      <Box sx={{display: "flex", alignItems: "center"}}>
+        <CircleCheckbox />
+
+        <Box sx={{flexGrow: 1}}>
+          <CardActionArea onClick={props.toggle} sx={{width: "100%"}}>
+            <CardContent>
+              <Typography variant="body1" color="text.primary" sx={{mb: "5px"}}>
+                {props.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {props.due_date} | {props.priority} | {props.label}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Box>
+
+        <IconButton aria-label="star">
+          <StarBorderOutlinedIcon />
+        </IconButton>
+
+      </Box>
+
+      <Divider component="li" />
+
+    </Card>
+  );
+}
