bc64f0c0ea38ed8d1f253ad26ff182d6c9520a92
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 10:41:48 -0400

    Add content to new task modal

--- frontend/src/components/NewTaskModal.js
+++ frontend/src/components/NewTaskModal.js
@@ -1,17 +1,27 @@
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import TagRoundedIcon from '@mui/icons-material/TagRounded';
+import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
+import EventOutlinedIcon from '@mui/icons-material/EventOutlined';
+import AccessAlarmOutlinedIcon from '@mui/icons-material/AccessAlarmOutlined';
 
 const newTaskModalStyle = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 600,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
-  p: 4,
+  p: 6
 };
 
 export default function NewTaskModal(props) {
@@ -20,20 +30,65 @@ export default function NewTaskModal(props) {
       open={props.isOpen}
       onClose={props.handleClose}
     >
-
       <Box sx={newTaskModalStyle}>
+        <Card elevation={0}>
 
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+          <CardHeader
+            title={
+              <TextField
+              fullWidth
+              id="task-name"
+              label="Task Name"
+              variant="outlined"
+            />}
+          />
 
+          <CardContent>
+            <TextField
+              fullWidth
+              id="description"
+              label="Description"
+              multiline
+              rows={4}
+              variant="outlined"
+            />
+          </CardContent>
+
+          <CardActions
+            sx={{display: "flex", justifyContent: "space-between"}}
+          >
+            <Box>
+              <IconButton aria-label="priority">
+                <TagRoundedIcon />
+              </IconButton>
+            </Box>
+            <Box>
+              <IconButton aria-label="priority">
+                <FlagOutlinedIcon />
+              </IconButton>
+              <IconButton aria-label="schedule">
+                <EventOutlinedIcon />
+              </IconButton>
+              <IconButton aria-label="reminder">
+                <AccessAlarmOutlinedIcon />
+              </IconButton>
+            </Box>
+
+          </CardActions>
+
+          <CardActions sx={{display: "flex", justifyContent: "flex-end"}}>
+              <Button variant="outlined" onClick={props.handleClose}>
+                Cancel
+              </Button>
+              <Button variant="contained">
+                Add Task
+              </Button>
+          </CardActions>
+
+
+        </Card>
       </Box>
 
     </Modal>
   )
 }
-
-
