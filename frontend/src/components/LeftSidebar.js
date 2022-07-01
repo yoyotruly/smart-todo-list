0964960ef5b35d2854fcba70706b4d8ed79d4799
Yoyo Yang <yoyo.truly@gmail.com>
2022-06-30 20:19:26 -0400

    Refactor menu list item into its own component

--- frontend/src/components/LeftSidebar.js
+++ frontend/src/components/LeftSidebar.js
@@ -1,171 +1,103 @@
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
 import TodayOutlinedIcon from '@mui/icons-material/TodayOutlined';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import AccessAlarmOutlinedIcon from '@mui/icons-material/AccessAlarmOutlined';
 import EventAvailableOutlinedIcon from '@mui/icons-material/EventAvailableOutlined';
 import SquareRoundedIcon from '@mui/icons-material/SquareRounded';
+import MenuListItem from './MenuListItem';
 
 const leftSidebarWidth = 240;
 
 export default function LeftSidebar() {
   return (
-      <Drawer
-        sx={{
+    <Drawer
+      sx={{
+        width: leftSidebarWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
           width: leftSidebarWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: leftSidebarWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
 
-        <Toolbar />
+      <Toolbar />
 
-        <List dense>
+      <List dense>
 
-          <ListItem
-            key={"Overview"}
-            component="a"
-            href="/tasks"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <SummarizeOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary="Today" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"Overview"}
+          href="/tasks"
+          text="Overview"
+          icon={<SummarizeOutlinedIcon />}
+        />
 
-          <ListItem
-            key={"Today"}
-            component="a"
-            href="/tasks/today"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <TodayOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary="Today" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"Today"}
+          href="/tasks/today"
+          text="Today"
+          icon={<TodayOutlinedIcon />}
+        />
 
-          <ListItem
-            key={"Scheduled"}
-            component="a"
-            href="/tasks/scheduled"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <AccessAlarmOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary="Scheduled" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"Scheduled"}
+          href="/tasks/scheduled"
+          text="Scheduled"
+          icon={<AccessAlarmOutlinedIcon />}
+        />
 
-          <ListItem
-            key={"Important"}
-            component="a"
-            href="/tasks/high"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <StarBorderOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary="Important" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"Important"}
+          href="/tasks/important"
+          text="Important"
+          icon={<StarBorderOutlinedIcon />}
+        />
 
-          <ListItem
-            key={"Done"}
-            component="a"
-            href="/tasks/done"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <EventAvailableOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText primary="Done" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"Done"}
+          href="/tasks/done"
+          text="Done"
+          icon={<EventAvailableOutlinedIcon />}
+        />
 
-        </List>
+      </List>
 
-        <List
-          dense
-          sx={{marginTop: "30px"}}
-        >
-          <ListItem
-            key={"To Eat"}
-            component="a"
-            href="/tasks/1"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <SquareRoundedIcon color='primary'/>
-              </ListItemIcon>
-              <ListItemText primary="To Eat" />
-            </ListItemButton>
-          </ListItem>
+      <List dense sx={{marginTop: "30px"}}>
 
-          <ListItem
-            key={"To Buy"}
-            component="a"
-            href="/tasks/2"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <SquareRoundedIcon color="warning"/>
-              </ListItemIcon>
-              <ListItemText primary="To Buy" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"To Eat"}
+          href="/tasks/1"
+          text="To Eat"
+          icon={<SquareRoundedIcon color='primary'/>}
+        />
 
-          <ListItem
-            key={"To Read"}
-            component="a"
-            href="/tasks/3"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <SquareRoundedIcon color="secondary"/>
-              </ListItemIcon>
-              <ListItemText primary="To Read" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"To Buy"}
+          href="/tasks/2"
+          text="To Buy"
+          icon={<SquareRoundedIcon color='warning'/>}
+        />
 
-          <ListItem
-            key={"To Watch"}
-            component="a"
-            href="/tasks/4"
-            sx={{color: "text.primary"}}
-          >
-            <ListItemButton>
-              <ListItemIcon>
-                <SquareRoundedIcon color="success"/>
-              </ListItemIcon>
-              <ListItemText primary="To Watch" />
-            </ListItemButton>
-          </ListItem>
+        <MenuListItem
+          key={"To Read"}
+          href="/tasks/3"
+          text="To Read"
+          icon={<SquareRoundedIcon color='secondary'/>}
+        />
 
-        </List>
-      </Drawer>
+        <MenuListItem
+          key={"To Watch"}
+          href="/tasks/4"
+          text="To Watch"
+          icon={<SquareRoundedIcon color='success'/>}
+        />
+      </List>
+
+    </Drawer>
   );
 }
---
+++ frontend/src/components/MenuListItem.js
@@ -0,0 +1,25 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function MenuListItem(props) {
+  return (
+    <ListItem
+      component="a"
+      href={props.href}
+      sx={{color: "text.primary"}}
+      >
+      <ListItemButton>
+        <ListItemIcon>
+          {props.icon}
+          {/* <SummarizeOutlinedIcon /> */}
+        </ListItemIcon>
+        <ListItemText primary={props.text} />
+      </ListItemButton>
+      </ListItem>
+  )
+}
+
+
+
