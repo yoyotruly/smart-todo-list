c75ff25f1c5f585abceb296b5a5d0b97b969557d
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-02 03:46:38 -0400

    Modify tasks seed data

--- backend/db/seeds/04_tasks.sql
+++ backend/db/seeds/04_tasks.sql
@@ -1,29 +1,14 @@
 INSERT INTO tasks
   (user_id, priority_id, label_id, title, description, due_date)
 VALUES
-  (1, 1, 2, 'Buy ugly vegetables from the Farmer''s Market this weekend', 'People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too. I need to save them.\n(Wow, I''m such a hero)', '2022-07-02');
+  (1, 1, 1, 'Buy ugly vegetables from the Farmer''s Market this weekend', 'People get so judgmental about how food looks. I mean, ugly, soft, discolored food is food, too. I need to save them.\n(Wow, I''m such a hero)', '2022-07-03');
 
 INSERT INTO tasks
-  (user_id, priority_id, label_id, title, description, is_complete)
+  (user_id, priority_id, label_id, title, description)
 VALUES
-  (1, 2, 4, 'Rewatch Hawk and Chick with Louise', 'Best. Movie. Ever.', TRUE);
-
-INSERT INTO tasks
-  (user_id, label_id, title, description)
-VALUES
-  (1, 3, 'Test Title', 'Test description.');
+  (1, 2, 4, 'Rewatch Hawk and Chick with Louise', 'Best. Movie. Ever.');
 
 INSERT INTO tasks
   (user_id, label_id, title, description)
 VALUES
-  (1, 3, 'Test Title', 'Test description.');
-
-INSERT INTO tasks
-  (user_id, label_id, title, description)
-VALUES
-  (1, 4, 'Test Title', 'Test description.');
-
-INSERT INTO tasks
-  (user_id, label_id, title, description)
-VALUES
-  (1, 4, 'Test Title', 'Test description.');
+  (1, 2, 'Try out that steakhouse by the lake', 'What''s people''s obessesion with steaks? They should be eating burgers. My burgers.');
