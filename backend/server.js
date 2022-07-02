345486d6f306abc837f68003343c28b6c5dd1e00
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-02 00:14:40 -0400

    Add body-parser import

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const PORT = process.env.PORT || 8080;
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 const morgan = require("morgan");
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
 app.use(cors({
