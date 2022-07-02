6a0c9a2bbaadf01c530d112a193ca1bda36a1273
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-01 23:48:09 -0400

    Change priorities and labels column names

--- backend/db/seeds/03_labels.sql
+++ backend/db/seeds/03_labels.sql
@@ -1,4 +1,4 @@
-INSERT INTO labels (label) VALUES ('To Eat');
-INSERT INTO labels (label) VALUES ('To Buy');
-INSERT INTO labels (label) VALUES ('To Read');
-INSERT INTO labels (label) VALUES ('To Watch');
+INSERT INTO labels (name) VALUES ('To Buy');
+INSERT INTO labels (name) VALUES ('To Eat');
+INSERT INTO labels (name) VALUES ('To Read');
+INSERT INTO labels (name) VALUES ('To Watch');
